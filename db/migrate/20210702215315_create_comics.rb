b7a07b62c93a5894eca97d4838a3772071d6e4da
luis <foreverluiscastro@gmail.com>
2021-07-08 15:36:29 -0400

    reset schema.

--- db/migrate/20210702215315_create_comics.rb
+++ db/migrate/20210702215315_create_comics.rb
@@ -1,4 +1,4 @@
-class CreateRecipes < ActiveRecord::Migration[6.1]
+class CreateComics < ActiveRecord::Migration[6.1]
   def change
     create_table :comics do |t|
       t.string :title
--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_02_215315) do
+ActiveRecord::Schema.define(version: 0) do
 
-  create_table "recipes", force: :cascade do |t|
-    t.string "title"
-    t.text "instructions"
-    t.integer "minutes_to_complete"
-    t.integer "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_recipes_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password_digest"
-    t.string "image_url"
-    t.string "bio"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  add_foreign_key "recipes", "users"
 end
